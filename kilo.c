2981f8af11a41f730b1ea5ec07e9b153857dd06d
Tyler Collingridge <tylercollingridge@gmail.com>
2024-11-15 11:37:11 -0700

    feat: create enableRawMode function
    - Gets terminal attributes, turns off echo, and apply them.
    - We are trying to render a UI in raw mode, so we do not want the
      terminal to be echoing out what we are typing.

--- kilo.c
+++ kilo.c
@@ -1,8 +1,22 @@
 #include <termios.h>
 #include <unistd.h>
 
+// get the terminal attr, turn off echo, and apply the attr to raw
+void enableRawMode()
+{
+  struct termios raw;
+
+  tcgetattr(STDIN_FILENO, &raw);
+
+  raw.c_lflag &= ~(ECHO);
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main()
 {
+  enableRawMode();
+
   char c;
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
     ;
