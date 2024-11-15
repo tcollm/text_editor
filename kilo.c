250ae916869f47a76ceaa81da4d81db2a6c79247
Tyler Collingridge <tylercollingridge@gmail.com>
2024-11-15 11:49:05 -0700

    feat: change main to print key presses
    - Prints control keys as well as letters.
    - (For demonstration purposes.)

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -10,17 +12,15 @@ void disableRawMode()
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
-// turn off echo (in terminal attributes) while using raw mode
+// turn off echo (output user input) and canonical (read byte
+// by byte instead of line by line) flag while using raw mode
 void enableRawMode()
 {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
-
-  tcgetattr(STDIN_FILENO, &raw);
-
-  raw.c_lflag &= ~(ECHO);
+  raw.c_lflag &= ~(ECHO | ICANON);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -31,6 +31,16 @@ int main()
 
   char c;
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
-    ;
+  {
+    // print control keypresses (arrows, backspace, etc.)
+    if (iscntrl(c))
+    {
+      printf("%d\n", c);
+    }
+    else
+    {
+      printf("%d ('%c')\n", c, c);
+    }
+  };
   return 0;
 }
