e813f83ac816a098ef4f1dfd57943a7d623116b1
Tyler Collingridge <tylercollingridge@gmail.com>
2024-11-15 12:23:27 -0700

    feat: disable auto handling of carriage returns and output processing

---
+++ README.md
@@ -0,0 +1,1 @@
+Note if using VSCode: In order for this program to work as intended, you must disable any keyboard shortcuts that use "ctrl" (or just avoid using them.)
--- kilo.c
+++ kilo.c
@@ -13,18 +13,24 @@ void disableRawMode()
 }
 
 // turn off while using raw mode:
+// - disable auto handling of carriage returns as newlines (ICRNL, make ctrl-m produce 13 instead of 10)
+// - software flow control (IXON, ctrl-s stops data transmitted until ctrl-q pressed)
+
+// - disable output processing (OPOST, so that multiple carriage returns and newlines are not output when enter is pressed)
+
 // - echo (output user input)
 // - canonical mode (read byte by byte instead of line by line)
+// - input str literals (IEXTEN, some systems allow you to type ctr-v to type str literal chars)
 // - signal interrupts (ctrl-c, ctrl-z)
-// - software flow ctrl (IXON, ctrl-s stops data transmitted until ctrl-q pressed)
 void enableRawMode()
 {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
-  raw.c_iflag &= ~(IXON);
-  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+  raw.c_iflag &= ~(ICRNL | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
